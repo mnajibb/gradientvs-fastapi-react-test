0751b5b30c643e0ea67be5fde4a2c18815a41315
Ahmad Alfan <ahmad@gradienttech.io>
2024-05-16 10:05:23 +0700

    fix: chakra ui core error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from 'react-dom';
-import { ChakraProvider } from "@chakra-ui/core";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import Header from "./Components/Header";
-import Todos from "./Components/Todos";
+import Header from './Components/Header';
+import Todos from './Components/Todos';
 
 function App() {
   return (
@@ -11,8 +11,9 @@ function App() {
       <Header />
       <Todos />
     </ChakraProvider>
-  )
+  );
 }
 
-const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
