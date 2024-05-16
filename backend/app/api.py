407f46f3819059d111cebd3a4a795bdb2c83f6c1
Ahmad Alfan <ahmad@gradienttech.io>
2024-05-16 10:04:44 +0700

    fix: inconsistent data and endpoint handler

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,18 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import uuid
 
+class Todo(BaseModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, alias="id")
+    item: str
 
-todos = [
-    {
-        "id": "1",
-        "item": "Read a book."
-    },
-    {
-        "id": "2",
-        "item": "Cycle around town."
-    }
-]
-
+class TodoMutation(BaseModel):
+    item: str
 
 app = FastAPI()
 
@@ -21,57 +18,47 @@ origins = [
     "localhost:3000"
 ]
 
-
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
+# Dummy database simulation
+db: List[Todo] = []
+db.append(Todo(item="Read a book."))
+db.append(Todo(item="Cycle around town."))
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
 
-
 @app.get("/todo", tags=["todos"])
 async def get_todos() -> dict:
-    return { "data": todos }
-
+    return {"data": db}
 
 @app.post("/todo", tags=["todos"])
-async def add_todo(todo: dict) -> dict:
-    todos.append(todo)
-    return {
-        "data": { "Todo added." }
-    }
-
+async def add_todo(todo: TodoMutation) -> dict:
+    new_todo = Todo(item=todo.item)
+    db.append(new_todo)
+    return {"data": f"Todo with id {str(new_todo.id)} added."}
 
 @app.put("/todo/{id}", tags=["todos"])
-async def update_todo(id: int, body: dict) -> dict:
-    for todo in todos:
-        if int(todo["id"]) == id:
-            todo["item"] = body["item"]
-            return {
-                "data": f"Todo with id {id} has been updated."
-            }
-
-    return {
-        "data": f"Todo with id {id} not found."
-    }
-
+async def update_todo(id: str, todo_update: TodoMutation) -> dict:
+    id = uuid.UUID(id)
+    for todo in db:
+        if todo.id == id:
+            todo.item = todo_update.item
+            return {"data": f"Todo with id {str(id)} has been updated."}
+    raise HTTPException(status_code=404, detail=f"Todo with id {str(id)} not found.")
 
 @app.delete("/todo/{id}", tags=["todos"])
-async def delete_todo(id: int) -> dict:
-    for todo in todos:
-        if int(todo["id"]) == id:
-            todos.remove(todo)
-            return {
-                "data": f"Todo with id {id} has been removed."
-            }
-
-    return {
-        "data": f"Todo with id {id} not found."
-    }
+async def delete_todo(id: str) -> dict:
+    id = uuid.UUID(id)
+    for i, todo in enumerate(db):
+        if todo.id == id:
+            db.pop(i)
+            return {"data": f"Todo with id {str(id)} has been removed."}
+    raise HTTPException(status_code=404, detail=f"Todo with id {str(id)} not found.")
